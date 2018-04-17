bf822852866b9a216209c10e74760fdd831c677b
vpodgurskiy <vpodgurskiy@gmail.com>
2018-04-17 17:24:25 +0300

    debug with eslint

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
 export const inputUserName = () => {
-    const userName = readlineSync.question('May I have your name? ');
-    console.log('Hi ' + userName + '!');
-}
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hi, ${userName}!`);
+};
+export default inputUserName;
